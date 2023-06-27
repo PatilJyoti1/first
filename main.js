7f7a23fab1fea02b2f2cc556bf335589340952b9
PatilJyoti1 <jyotipatil616861@gmail.com>
2023-06-27 12:32:28 +0000

    firstcommit

---
+++ main.js
@@ -0,0 +1,80 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+
+// Form submit event
+
+form.addEventListener('submit', addItem);
+
+//delete event
+itemList.addEventListener('click',removeItem);
+
+//filter event
+filter .addEventListener('keyup', filterItems);
+// Add item
+
+function addItem(e){
+    e.preventDefault();
+
+// Get input value
+var newItem = document.getElementById('item').value;
+
+// Create new li element
+var li = document.createElement('li');
+
+// Add class
+li.className = 'list-group-item';
+
+// Add text node with input value
+li.appendChild(document.createTextNode(newItem));
+
+// Create 
+
+ var deleteBtn = document.createElement('button');
+
+// Add classes to del button
+
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+// Append text node
+deleteBtn.appendChild(document.createTextNode('X'));
+
+// Append button to li
+
+li.appendChild(deleteBtn);
+
+// Append li to list
+
+itemList.appendChild(li);
+
+}
+
+// Remove item
+
+function removeItem(e){ 
+    if(e.target.classList.contains('delete')){ 
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+
+}
+//filter
+function filterItems(e) {
+    var text = e.target.value.toLowercase();
+    itemList.getElementsByTagName('li');
+    
+
+    Array.from(items).forEach(function(items) {
+        
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowercase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none'
+        }
+    });
+
+}
